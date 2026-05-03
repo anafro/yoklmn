1f5836b864e629dca71c5e414c36e623d8ea61af
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-05-03 18:30:07 +1000

    feat: add word existence methods

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    public static function exists(string $word): bool
+    {
+        return self::query()->where('string', $word)->exists();
+    }
+
+    public static function doesntExist(string $word): bool
+    {
+        return ! self::exists($word);
+    }
 }
