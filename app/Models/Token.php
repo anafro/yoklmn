c58feb0c30e3c8cb4386c88019e5700bf01dc22e
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-05-03 18:29:52 +1000

    feat: add `Token::random()`

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Token extends Model
 {
-    //
+    public static function random(): string
+    {
+        return self::query()->inRandomOrder()->first()->string;
+    }
 }
